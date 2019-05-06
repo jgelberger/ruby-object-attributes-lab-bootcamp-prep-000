ac1df3e5e1e978b7f68cacfe62c826a1604bc4f1
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2019-05-06 00:48:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,22 +16,4 @@ class Dog
     @breed
   end
   
-class Person
-  
-  def name=(persons_name)
-    @name = persons_name
-  end
-  
-  def name
-    @name
-  end
-  
-  def job=(persons_job)
-    @job = persons_job
-  end
-  
-  def job
-    @job
-  end
-
 end
